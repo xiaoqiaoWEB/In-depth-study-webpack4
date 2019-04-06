0e9c84f23ca115728910fa6602596e4cef32bd31
xiaoqiaoWEB <346043591@qq.com>
2019-04-06 23:36:55 +0800

    one

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-var img = require('./ll.jpg')
-var img1 = require('./123.jpg')
-let css = require('./index.css')
-// let bg = require('./bg.less')
-import bg from './bg.less'
-
-let mm = new Image();
-mm.src = img;
-mm.classList.add('title')
-
-let root = document.getElementById('root');
-root.append(mm)
+console.log('A')
